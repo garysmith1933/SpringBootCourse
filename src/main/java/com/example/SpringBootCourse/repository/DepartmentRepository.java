d2215211932cca1fac532d98ae717b4492444c78
Gary Smith <garysmith193358@gmail.com>
2024-01-30 17:37:06 -0500

    added method to ignore casing when looking for department name

--- src/main/java/com/example/SpringBootCourse/repository/DepartmentRepository.java
+++ src/main/java/com/example/SpringBootCourse/repository/DepartmentRepository.java
@@ -9,4 +9,5 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
  //has access to all Jpa Repository methods. ex: save, findAll, findById
 
     public Department findByDepartmentName(String departmentName); // have to define it here as it does not exist in the repository.
+    public Department findByDepartmentNameIgnoreCase(String departmentName);
 }
