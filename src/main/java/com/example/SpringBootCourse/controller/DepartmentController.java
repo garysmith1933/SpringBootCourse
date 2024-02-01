8d4f68e2e6c08212851e6129a6ca57487edad104
Gary Smith <garysmith193358@gmail.com>
2024-02-01 18:43:05 -0500

    added loggers

--- src/main/java/com/example/SpringBootCourse/controller/DepartmentController.java
+++ src/main/java/com/example/SpringBootCourse/controller/DepartmentController.java
@@ -3,11 +3,12 @@ package com.example.SpringBootCourse.controller;
 import com.example.SpringBootCourse.entity.Department;
 import com.example.SpringBootCourse.service.DepartmentService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController // this is where api methods are defined
 public class DepartmentController {
@@ -15,14 +16,18 @@ public class DepartmentController {
     @Autowired // maps this departmentService to the one that is already in the spring container.
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger( DepartmentController.class);
+
     @PostMapping("/departments")
     //the input will take the body of the request and convert it to be a department
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of Department Controller")
         return departmentService.fetchDepartments();
     }
 
--- src/main/java/com/example/SpringBootCourse/entity/Department.java
+++ src/main/java/com/example/SpringBootCourse/entity/Department.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class Department {
