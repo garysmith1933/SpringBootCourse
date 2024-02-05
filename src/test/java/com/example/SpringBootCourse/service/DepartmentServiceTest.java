c62327e818a58b1cda328987093222cd5234ac42
Gary Smith <garysmith193358@gmail.com>
2024-02-05 07:39:28 -0500

    set up mock bean department

--- src/test/java/com/example/SpringBootCourse/service/DepartmentServiceTest.java
+++ src/test/java/com/example/SpringBootCourse/service/DepartmentServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.SpringBootCourse.service;
 
 import com.example.SpringBootCourse.entity.Department;
+import com.example.SpringBootCourse.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,8 +16,14 @@ class DepartmentServiceTest {
     @Autowired
     private DepartmentService departmentService;
 
+    @MockBean // the mocks the actions the object would do, but the data is not actually saved.
+    private DepartmentRepository departmentRepository;
+
     @BeforeEach
     void setUp() {
+        Department department = Department.builder().departmentName("IT").departmentAddress("Ewing").departmentCode("IT-01").departmentId(1L).build();
+        //when the method below is called, return the department
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT")).thenReturn(department);
     }
 
     @Test
