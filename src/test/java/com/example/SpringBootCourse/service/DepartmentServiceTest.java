b63c871defeb50949b4385281f4bac38702a0715
Gary Smith <garysmith193358@gmail.com>
2024-02-05 07:24:20 -0500

    added test for department name method

---
+++ src/test/java/com/example/SpringBootCourse/service/DepartmentServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.SpringBootCourse.service;
+
+import com.example.SpringBootCourse.entity.Department;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+    @Autowired
+    private DepartmentService departmentService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void whenValidDepartmentName_thenDepartmentShouldFound() {
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
